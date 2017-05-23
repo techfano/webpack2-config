068ee137c43f9325160d62d97826419f73ee94ba
Alexandro Estefano Castañeda Bello <ecastaneda@Mac-mini-de-ECASTANEDA.local>
2017-05-23 14:45:31 -0500

    adding html template generator

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: './dist/app.bundle.js'
-  }
+    path: __dirname +'/dist',
+    filename: 'app.bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'webpack2 config demo',
+    minify: {
+      collapseWhitespace: true
+    },
+    hash: true,
+    template: 'src/index.ejs'
+  })]
 }
